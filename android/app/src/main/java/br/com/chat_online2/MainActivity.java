d3267c877cdd9c4dc9da9629fdee09be43b63041
GaGuarGo <gabrielguarnierigomes@gmail.com>
2023-08-16 12:49:41 -0300

    First Commit

---
+++ android/app/src/main/java/br/com/chat_online2/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.chat_online2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          iconTheme: IconThemeData(color: Colors.lightBlueAccent)),
+      home: ChatScreen(),
+    );
+  }
+}
